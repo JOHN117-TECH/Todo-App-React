c401d733806c3308bc1623e6163434741b423493
John <barretodiazjohnalexander@gmail.com>
2023-10-05 16:51:20 -0500

    ajuste pantalla mobile

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react';
+import {useState } from 'react';
 import './App.css';
 import CreateTask from './components/CreateTask';
 import Header from './components/layouts/Header';
